fd8d5fde72460a37168db43ade7c3cf088ff291e
Lorttexwolf <comisgammer@gmail.com>
2021-03-01 17:13:20 -0600

    Update Patch_Progression.cs

--- MTFO/Patches/Patch_Progression.cs
+++ MTFO/Patches/Patch_Progression.cs
@@ -4,17 +4,20 @@ using MTFO.Utilities;
 using ChainedPuzzles;
 using UnityEngine;
 using Newtonsoft.Json;
+using BepInEx;
+using System.IO;
+using DropServer;
 
 namespace MTFO.Patches
 {
-    [HarmonyPatch(typeof(DropServerManager), nameof(DropServerManager.GetRundownProgressionAsync))]
+    [HarmonyPatch(typeof(RundownManager), nameof(RundownManager.OnRundownProgressionRequestDone))]
     internal static class Patch_RundownProgression
     {
 
-        internal static void Prefix()
+        internal static void Postfix()
         {
 
-
+            File.WriteAllText(Path.Combine(Paths.ConfigPath, "progression.json"), JsonConvert.SerializeObject(RundownManager.RundownProgression));
 
         }
 
